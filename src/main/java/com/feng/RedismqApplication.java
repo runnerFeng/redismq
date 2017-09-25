0a4aab12c2b8bf989e33c7c34080dd42827471f1
liufeng <631819061@qq.com>
2017-09-25 10:51:32 +0800

    update

---
+++ src/main/java/com/feng/RedismqApplication.java
@@ -0,0 +1,12 @@
+package com.feng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedismqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedismqApplication.class, args);
+    }
+}
